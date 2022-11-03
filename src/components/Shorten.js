b8bde031fa35ffa417ef00d0fd5b521d26c2eecc
Muhamad Handika <muhammadhandika78@gmail.com>
2022-11-03 09:44:42 +0700

    Fixed padding in shorten form

--- src/components/Shorten.js
+++ src/components/Shorten.js
@@ -4,7 +4,7 @@ function Shorten() {
       id='shorten'
       className=' section-padding mt-32 py-3 bg-neutral-light'
     >
-      <form className='-mt-20 bg-primary-dark-violet bg-shorten-mobile bg-no-repeat bg-right-top py-10 px-9 flex flex-col gap-5 justify-between items-center rounded-xl lg:bg-shorten-desktop lg:bg-cover lg:flex-row'>
+      <form className='-mt-20 bg-primary-dark-violet bg-shorten-mobile bg-no-repeat bg-right-top py-6 px-5 flex flex-col gap-5 justify-between items-center rounded-xl lg:bg-shorten-desktop lg:bg-cover lg:flex-row lg:py-10 lg:px-9'>
         <input
           type='text'
           placeholder='Shorten a link here...'
